81edcee659042cef6a8d46d2a55696f77e8ac67c
MatildeDiprima <matilded239@gmail.com>
2023-03-20 11:26:37 +0100

    add seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,14 +4,29 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+    private $categories = [
+        [
+            "name"=> "Uomo"
+        ],
+        [
+            "name"=> "Donna"
+        ],
+        [
+            "name"=> "Bambino"
+        ],
+    ];
+
     public function run(): void
     {
-        //
+        foreach($this-> categories as $category)
+        {
+            Category::create([
+                'name'=> $category['name']
+            ]);
+        }
     }
 }
