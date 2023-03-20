9673afdcf550006b06a072f9314e5511e7e741c9
MatildeDiprima <matilded239@gmail.com>
2023-03-20 17:18:04 +0100

    add 3 products with lower price

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,5 +3,20 @@
 @section('head_title', 'Homepage')
 
 @section('content')
-    <h1>Prodotti in offerta</h1>
+    <h1 class="mb-4">Prodotti in offerta</h1>
+    <div class="container text-center d-inline-flex" style="height:100%; "> 
+        <div class="container row">
+            @foreach($products as $product)
+            <div class="card col-4"style="width: 20rem;">
+                <img src="{{ $product->image }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{substr($product->title,0,40 )}}</h5>
+                    <p class="card-text">{{ substr($product->description,0,80 )}}</p>
+                    <h3 class="card-price">{{ $product->price }}</h3>
+                    <a href="#" class="btn btn-primary">Aggiungi al carrello</a>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
