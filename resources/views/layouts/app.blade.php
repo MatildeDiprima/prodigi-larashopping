e4154bf62b571cc172db4acc8eb37718a02cf9f7
MatildeDiprima <matilded239@gmail.com>
2023-03-20 13:39:50 +0100

    create folder 'layouts' and file 'app'

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>@yield('head_title')</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.6.4.js"></script>
+        <style>
+            a{
+                text-decoration: none;
+                color: black;
+            }
+        </style>
+    </head>
+    
+    <body>
+        @include('partials.navbar')
+        <div class="container mt-5">
+            @yield('content')
+        </div>
+    </body>
+</html>
